687dbfd19784de0df2efe1f445b855666b01f9b6
thuggys <150315417+thuggys@users.noreply.github.com>
2025-01-27 08:02:12 -0500

    Add "use client" directive to interactive components and layout
    * **components/interactive/quiz.tsx**
      - Add `"use client"` directive at the top of the file
    
    * **components/interactive/code-editor.tsx**
      - Add `"use client"` directive at the top of the file
    
    * **app/docs/layout.tsx**
      - Add `"use client"` directive at the top of the file

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Sidebar } from "@/components/navigation/sidebar"
 import Quiz from "@/components/interactive/quiz"
 import CodeEditor from "@/components/interactive/code-editor"
